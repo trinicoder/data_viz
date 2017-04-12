b981a36499555cfb670c9b38ffa1a82b07418098
Varoon Silochan <zeter90@yahoo.com>
2017-04-11 21:12:33 -0400

    updated query statments, form changes

--- hello.py
+++ hello.py
@@ -68,7 +68,9 @@ class populationdata(db.Model):
 class populationRequest(Form):
     year = IntegerField('Enter Year (1950 and 2100)', validators=[Required()])
     age = IntegerField('Enter age (0-100)', validators=[Required()])
-    #country = SelectField('Selct a country')
+    gender = SelectField('Select Gender', choices=[('Male', 'Male'), ('Female', 'Female')])
+    country = SelectField('Select a country', choices=[('Guyana', 'Guyana'), 
+                            ('Jamaica', 'Jamaica'), ('Trinidad and Tobago', 'Trinidad and Tobago')])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -78,39 +80,36 @@ def index():
 
 @app.route('/data')
 def data():
-    print (session.get('data'))
-    print (session.get('pop_io'))
-    return render_template('index2.html',  pop_data=session.get('pop_io'))
+    print(session.get('data'))
+    print(session.get('pop_io'))
+    return render_template('index2.html', pop_data=session.get('pop_io'))
 
 
 @app.route('/query', methods=['GET', 'POST'])
 def query():
     # session['data'] = ['foobar']
+    #session['test'] = 'laddy'
     form = populationRequest()
     if form.validate_on_submit():
         year = form.year.data
         age = form.age.data
-        getRequest(year, age)
-        session['data'] = [year, age]
-        return redirect(url_for('index'))
+        country = form.country.data
+        gender = form.gender.data
+
+        c= Country.query.filter_by(countryname= country).first()
+        c2= populationdata.query.filter_by(countryid=c.countryid,age=age,year=year).first()
+        session['country'] = country
+       
+        session['query']= c2
+        #session['query'] = [(e.populationid,e.countryid,e.age,e.male,e.female,e.year)
+        print(c2)
+     
+        return redirect(url_for('query'))
     # return render_template('query.html', form=form, pop_data=session.get('pop_io'))
-    result=Country.query.all()
-    return render_template('query.html', form=form,query=result)
-    
-def getRequest(year, age):
-        print (year,age)
-        #r = requests.get('http://api.population.io:80/1.0/population/'+ str(year)+ '/aged/'+ str(age)+ '/')
-        #r = requests.get('http://swapi.co/api/people/1/')
-       # data = r.json()
-        data ={'name': 'Luke Skywalker', 'height': '172', 'mass': '77', 'hair_color': 'blond', 'skin_color': 'fair', 'eye_color': 'blue',
-         'birth_year': '19BBY', 'gender': 'male', 'homeworld': 'http://swapi.co/api/planets/1/', 'films': ['http://swapi.co/api/films/6/', 
-         'http://swapi.co/api/films/3/', 'http://swapi.co/api/films/2/', 'http://swapi.co/api/films/1/', 'http://swapi.co/api/films/7/'], 
-         'species': ['http://swapi.co/api/species/1/'], 'vehicles': ['http://swapi.co/api/vehicles/14/', 'http://swapi.co/api/vehicles/30/'],
-          'starships': ['http://swapi.co/api/starships/12/', 'http://swapi.co/api/starships/22/'], 'created': '2014-12-09T13:50:51.644000Z', 
-          'edited': '2014-12-20T21:17:56.891000Z', 'url': 'http://swapi.co/api/people/1/'}
-        #data= ['derp', 'lerp']
-        print (data)
-        session['pop_io']= data
+   # result = Country.query.all()
+    return render_template('query.html', form=form, country=session.get('country'))
+
+
 
 if __name__ == '__main__':
     db.create_all()
